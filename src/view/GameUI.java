260ba8e18c8a944641166559f826f572b73b8425
jqvistt <joakim.is.lonnqvist@gmail.com>
2025-10-15 00:09:49 +0300

    Add working drag & drop
    The drag and drop functionality works now. Only thing missing is styling and adding a temporary panel for the cursor when dragging.

--- src/view/GameUI.java
+++ src/view/GameUI.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,8 +28,10 @@ public class GameUI extends JPanel
 	
 	private static final Color BG = Color.BLACK;
 	private static final Color CELL_COLOR = Color.WHITE;
+	private static final Color HOVERED_CELL_COLOR = Color.LIGHT_GRAY;
 
 	private JButton currentCursorItem = null; 
+	private JPanel currentHoveredCell = null;
 	private List<JButton> numberButtons = new ArrayList<>();
 
 	
@@ -53,6 +57,7 @@ public class GameUI extends JPanel
 				
 			JPanel cell = new JPanel();
 			cell.setBackground(CELL_COLOR);
+			cell.setFont(new Font("Broadway", Font.BOLD, 45));
 			
 			int top = (i % 3 == 0) ? 5 : 1;
         	int left = (j % 3 == 0) ? 5 : 1;
@@ -62,19 +67,20 @@ public class GameUI extends JPanel
 			cell.setBorder(BorderFactory.createMatteBorder(top, left, bottom, right, Color.BLACK));
 			gameBoardPanel.add(cell);
 
-			cell.addMouseListener(new java.awt.event.MouseAdapter() {
-				public void mouseReleased(java.awt.event.MouseEvent evt) {
-					if(currentCursorItem != null){
-						System.out.println("dropped number: " + currentCursorItem.getText() + " into cell");
-						cell.removeAll();
-						cell.add(new JLabel(currentCursorItem.getText()));
-						cell.revalidate();
-						cell.repaint();
-						currentCursorItem = null;
-					} else return;
-
+			cell.addMouseListener(new MouseAdapter() {
+				
+				public void mouseEntered(MouseEvent arg0) {
+					currentHoveredCell = cell;
+					cell.setBackground(HOVERED_CELL_COLOR);
 				}
-            });
+				
+				public void mouseExited(MouseEvent arg0) {
+					currentHoveredCell = null;
+					cell.setBackground(CELL_COLOR);
+				}
+				
+			});
+			
 			}
 		}
 		
@@ -87,7 +93,7 @@ public class GameUI extends JPanel
 		for (int i = 1; i <= 9;  i++){
 			
 			JButton numberButton = new JButton(String.valueOf(i));
-			numberButton.setFont(new Font("Broadway", Font.BOLD, 20));
+			numberButton.setFont(new Font("Broadway", Font.BOLD, 45));
 			numberButton.setBackground(Color.GRAY);
 			numberButton.setPreferredSize(new Dimension(WINDOW_SIZE_X / CELL_COUNT, WINDOW_SIZE_X / CELL_COUNT));
 			numberButton.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
@@ -95,24 +101,50 @@ public class GameUI extends JPanel
 			numberButton.setContentAreaFilled(false);
 			numberButton.setOpaque(true);
 			numberDeckPanel.add(numberButton);
-			numberButtons.add(numberButton);
-
-			numberButton.addMouseListener(new java.awt.event.MouseAdapter() {
-    		public void mousePressed(java.awt.event.MouseEvent evt) {
-			if(currentCursorItem == null){
-				System.out.println("picked up number: " + numberButton.getText());
-				currentCursorItem = numberButton;
-				numberButton.setBackground(Color.YELLOW); // Visual feedback when picked up
-			} else return;
-			}
-		});
-		}
-		
+			
+			numberButton.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mousePressed(MouseEvent arg0) {
+					pickUpNumber(numberButton);
+				}
+				public void mouseReleased(MouseEvent arg0) {
+					dropNumberIntoCell(currentHoveredCell);
+				}
+			});
+		} 
 
 		gameFrame.add(gameBoardPanel, BorderLayout.CENTER);
 		gameFrame.add(numberDeckPanel, BorderLayout.SOUTH);
 		gameFrame.setVisible(true);
 }
+	
+	
+	
+	
+	private void pickUpNumber(JButton numberButton) {
+		
+		if(currentCursorItem != null) return;
+		System.out.println("picking up number: " + numberButton.getText());
+		currentCursorItem = numberButton;
+		
+	}
+	
+	private void dropNumberIntoCell(JPanel targetCell) {
+		
+		if(targetCell == null || currentCursorItem == null) {
+			currentCursorItem = null;
+			System.err.println("no cell detected"); 
+			return;
+			}
+		
+		System.out.println("dropping number: " + currentCursorItem.getText() + " into cell");
+		targetCell.removeAll();
+		targetCell.add(new JLabel(currentCursorItem.getText()));
+		targetCell.revalidate();
+		targetCell.repaint();
+		currentCursorItem = null;
+		
+	}
 
 }
 
